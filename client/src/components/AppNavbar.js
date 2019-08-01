bb4776ac12348db990dfc62817ea52f96398b1bb
Chris <chrislo.7@live.com>
2019-08-01 19:19:24 -0400

    fixed typo in repository link

--- client/src/components/AppNavbar.js
+++ client/src/components/AppNavbar.js
@@ -31,7 +31,7 @@ class AppNavbar extends Component {
                         <Collapse isOpen={this.state.isOpen} navbar>
                             <Nav className="ml-auto" navbar>
                                 <NavItem>
-                                    <NavLink href="https://github.com/chrislo7/mern-todo">
+                                    <NavLink href="https://github.com/chrislo7/mern_todo">
                                         Github Repo
                                     </NavLink>
                                 </NavItem>
