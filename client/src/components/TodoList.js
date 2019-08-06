bb6734e1da747df9410576e6ca8d95bd6f0dd4c1
Chris <chrislo.7@live.com>
2019-08-06 18:43:47 -0400

    dragged out inline functions

--- client/src/components/TodoList.js
+++ client/src/components/TodoList.js
@@ -14,6 +14,21 @@ class TodoList extends Component {
         ]
     }
 
+    handleAddTodo = () => {
+        const name = prompt('Enter Todo');
+        if ( name ) {
+            this.setState(state => ({ 
+                todos: [...state.todos, { id: uuid(), name }]
+            }));
+        };
+    }
+
+    handleRemoveTodo = (id) => {
+        this.setState(state => ({
+            todos: state.todos.filter(todo => todo.id !== id )
+        }));
+    }
+
     render() {
         const { todos } = this.state;
         return(
@@ -21,17 +36,29 @@ class TodoList extends Component {
                 <Button 
                     color="dark" 
                     style={{ marginBottom: '2rem' }} 
-                    onClick={() => { 
-                        const name = prompt('Enter Todo');
-                        if ( name ) {
-                            this.setState(state => ({ 
-                                todos: [...state.todos, { id: uuid(), name }]
-                            }))
-                        }
-                    }}
+                    onClick= { this.handleAddTodo }
                 >
-                    Add Item
+                    Add Todo
                 </Button>
+                <ListGroup>
+                    <TransitionGroup className='todo-list'>
+                        {todos.map(({ id, name }) => (
+                            <CSSTransition key={ id } timeout={ 500 } classNames="fade">
+                                <ListGroupItem>
+                                    <Button 
+                                        className='remove-btn'
+                                        color='danger'
+                                        size='sm'
+                                        onClick={() => this.handleRemoveTodo(id) }
+                                    >
+                                        X
+                                    </Button>
+                                    {name}
+                                </ListGroupItem>
+                            </CSSTransition>
+                        ))}
+                    </TransitionGroup>
+                </ListGroup>
             </Container>
         )
     }
